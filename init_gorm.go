81f10d154e96d26246ec8aa332c06cde271bbd44
Jiujiang Yuan <jiujiangyuan@qq.com>
2019-04-20 23:08:28 +0800

    format

--- init_gorm.go
+++ init_gorm.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 var DefaultDB *gorm.DB
@@ -11,11 +12,11 @@ var DefaultDB *gorm.DB
 func InitGorm(dsn string) error {
 	db, err := gorm.Open("mysql", dsn)
 	if err != nil {
-		fmt.Printf("[Warn] gorm init from dsl[%s] errr[%s]", dsn, err)
+		fmt.Printf("[Warn] gorm init from dsl[%s] errr[%s]\n", dsn, err)
 		return err
 	}
 
 	DefaultDB = db
-	fmt.Printf("[Info] gorm init from dsl[%s] success", dsn)
+	fmt.Printf("[Info] gorm init from dsl[%s] success\n", dsn)
 	return nil
 }
